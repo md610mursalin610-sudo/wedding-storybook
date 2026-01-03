636376288a382235c9bd22a3d6db5af30ac49c37
md610mursalin610-sudo <md610mursalin610@gmail.com>
2026-01-02 22:03:43 -0800

    ALlow gmail add

--- src/config/allowedEmails.ts
+++ src/config/allowedEmails.ts
@@ -1,3 +1,6 @@
 export const ALLOWED_EMAILS: string[] = [
-  "you@example.com"
+  "you@example.com",
+  "md610mursalin610@gmail.com",
+  "abushaidislam7@gmail.com",
+  "shebaahmed049@gmail.com",
 ];
